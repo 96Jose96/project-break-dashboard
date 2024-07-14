cb160baf3288515fbfdde3a88fefd2d17b75e49d
Jose <jm.diaz6620@gmail.com>
2024-07-14 14:39:27 +0200

    clock start

--- Clock and date/clock.js
+++ Clock and date/clock.js
@@ -1,2 +1,11 @@
-newDate()
-console.log(newDate())
+let date = new Date()
+console.log(date)
+
+const clock = document.getElementById('clock')
+
+const fecha = () => {
+    setInterval(
+        clock.innerHTML = date, 1000
+    )
+}
+
