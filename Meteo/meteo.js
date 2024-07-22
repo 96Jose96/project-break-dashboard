2e77fb01eb59ae8fdd7d69dbf0bcfd576ef196b6
Jose <jm.diaz6620@gmail.com>
2024-07-22 13:29:23 +0200

    currect weather finish

--- Meteo/meteo.js
+++ Meteo/meteo.js
@@ -1,11 +1,58 @@
-url = 'https://api.weatherapi.com/v1/forecast.json?key=812f1314d5e54007a3d92843241907&q=madrid&aqi=no'
-const country = document.getElementById('country')
+document.addEventListener('DOMContentLoaded', () => {
+    const city = 'la zubia'
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=812f1314d5e54007a3d92843241907&q=${city}&aqi=no`;
 
-const getWeather = async () => {
-    const response = await fetch(url);
-    const weather = await response.json();
-    return weather;
-}
+    const getWeather = async () => {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Error', response.status);
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.log('Error al obtener datos', error);
+        }
+    };
 
-getWeather().then(data => console.log(data))
+    const currentWeather = (data) => {
+        const currentContainer = document.getElementById('currentcontainer');
+        const divTemp = document.getElementById('divtemp');
+
+        const location = `${data.location.name} / ${data.location.country}`;
+        const currentCondition = data.current.condition.text;
+        const currentTempIcon = data.current.condition.icon;
+        const currentTemp = `${data.current.temp_c} ºC`;
+        const humidity = `Humedad: ${data.current.humidity}%`
+        const precip = `Precipitaciones: ${data.current.precip_in}%`
+        const wind = `Viento ${data.current.wind_kph} Km/h`
+
+        console.log(wind);
+
+        const currentTemplate = 
+            `
+            <div class="location">
+                <span>${location}</span>
+                <span>${currentCondition}</span>
+                <img src=${currentTempIcon} />
+            </div>
+            <div class="tempconditions">
+                <div class="currenttemp">
+                    <span>${currentTemp}</span>
+                </div>
+                <div class="weatherconditions">
+                    <span>${humidity}</span>
+                    <span>${precip}</span>
+                    <span>${wind}</span>
+                </div>
+            </div>    
+            `
+
+        currentContainer.innerHTML = currentTemplate;
+    };
+    getWeather().then((data) => console.log(data))
+    getWeather().then((data) => currentWeather(data));
+    
+    
+});
 
